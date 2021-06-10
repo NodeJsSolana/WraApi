d2e54edc1929b6cd6a411223b850dc34ac3564b8
Mooncat <48311280+klas9802@users.noreply.github.com>
2021-06-10 18:23:08 +0800

    Update Water.tsx

--- my-api/src/components/Water.tsx
+++ my-api/src/components/Water.tsx
@@ -1,6 +1,7 @@
-import { IonContent,IonList, IonItem, IonLabel, IonListHeader} from '@ionic/react';
+import { IonContent,IonList, IonItem, IonLabel, IonListHeader, IonHeader, IonToolbar, IonTitle, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonIcon, IonItemSliding, IonItemOptions, IonItemOption, IonCardContent, IonButton, IonPage, IonGrid, IonRow, IonCol} from '@ionic/react';
 import React, { Component} from 'react';
 import axios from 'axios';
+import "./Water.css"
 
 
 class Water extends Component{
@@ -8,41 +9,71 @@ class Water extends Component{
     // API_URL = 'https://fhy.wra.gov.tw/WraApi/v1/Reservoir/RealTimeInfo?$top=30'
 
     state = {
-        persons: [],
+        item: [],
+        item2: [],
+        item3: []
     }
 
     componentDidMount() {
-        axios.get(`https://fhy.wra.gov.tw/WraApi/v1/Water/Station`)
+        axios.get(`https://fhy.wra.gov.tw/WraApi/v1/Reservoir/Station?$orderby=StationNo`)
         .then(res => {
         console.log(res)
-        const persons = res.data;
-        this.setState({ persons });
+        const item = res.data;
+        this.setState({ item });
         })
+
+        axios.get(`https://fhy.wra.gov.tw/WraApi/v1/Reservoir/Daily?$orderby=StationNo`)
+        .then(res => {
+        console.log(res)
+        const item2 = res.data;
+        this.setState({ item2 });
+        })
+        
     }
 
     render(){
-
-        
-
         return(
             <IonContent>
-                <IonListHeader>
-                    <IonLabel><h1>測站名稱</h1></IonLabel>
-                </IonListHeader>
-                
-                <IonList>
-                    <IonItem>
-                        <IonLabel>
-                            {
-                                this.state.persons.map((post:any, key) =>{
-                                return(
-                                    <li key={key}>{post.StationName}</li>
-                                )
-                                })
-                            }
-                        </IonLabel>
-                    </IonItem>
-                </IonList>    
+                <IonGrid>
+                    <IonRow>
+                        <IonCol>
+                            <div>
+                                <h5>水庫名稱</h5>
+                                {
+                                    this.state.item.map((post:any, key) =>{
+                                        return(
+                                            <li key={key}>{post.StationName}</li>
+                                            )
+                                        })
+                                }
+                            </div>
+                        </IonCol>
+                        <IonCol>
+                            <div>
+                                <h5>本日總進水量(萬立方公尺) </h5>
+                                {
+                                    this.state.item2.map((post:any, key) =>{
+                                        return(
+                                            <li key={key}>{post.InflowTotal}</li>
+                                            )
+                                        })
+                                }
+                            </div>
+                        </IonCol>
+                        <IonCol>
+                            <div>
+                                <h5>本日總出水量(萬立方公尺)</h5>
+                                {
+                                    this.state.item2.map((post:any, key) =>{
+                                        return(
+                                            <li key={key}>{post.OutflowTotal }</li>
+                                            )
+                                        })
+                                }
+                            </div>
+                        </IonCol>
+                    </IonRow>
+                </IonGrid>
             </IonContent>
         )
     }
@@ -50,3 +81,11 @@ class Water extends Component{
 }
 
 export default Water;
+
+// {
+//     this.state.item.map((post:any, key) =>{
+//     return(
+//         <li key={key}>{post.StationName}</li>
+//         )
+//     })
+// }
